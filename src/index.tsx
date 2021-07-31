37383a01f65a34b4ab3cc95acabc52dbbee5a053
vakrilov <alexander.vakrilov@gmail.com>
2021-07-31 22:55:12 +0300

    prevent scrolling

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+window.addEventListener("scroll", (e) => {
+  e.preventDefault();
+  window.scrollTo(0, 0);
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
